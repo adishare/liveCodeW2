11bbc179730873c052f1c5c4b390540a23aa85ac
adishare <adie.share@gmail.com>
2018-10-11 08:59:39 +0700

    template

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect('mongodb://localhost:27017/lcw2',{ useNewUrlParser: true })  //--------------
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors())
+
+app.use('/', require('./routes'));
+
+app.use(function(req, res, next) {
+	next(new Error("Not Found").status(404))
+})
+
+module.exports = app;
